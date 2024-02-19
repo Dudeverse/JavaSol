93aeb45c78a5c55b65958b5d0d3cae94c947e2e0
Dudeverse <se352@njit.edu>
2024-02-19 11:25:00 -0500

    added example

---
+++ ch03/Example_3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Example_3 {
+
+	public static void main(String[] args) {
+		int x = 7;
+		int y = 3;
+		x = y++;
+		System.out.printf("after calc: %d%n", x);
+		
+		x = 7;
+		y=3;
+		x = ++y;
+		System.out.printf("after calc: %d%n", x);
+
+
+
+		
+		
+				
+	}	
+
+}
