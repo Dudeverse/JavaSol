b28d94d40fa603e887731413f97f4263c9b9f703
Dudeverse <se352@njit.edu>
2024-02-20 04:07:07 -0500

    add 3.29 a and b

---
+++ ch03/Prob3_29a.java
@@ -0,0 +1,23 @@
+
+public class Prob3_29a {
+
+	public static void main(String[] args) {
+		
+		int x = 5;
+		int y = 8;
+		
+		if (y == 8) {
+			
+			 if (x == 5) {
+				 System.out.println("@@@@@");
+			 } else {
+				 System.out.println("#####");
+			 }
+			 
+			 System.out.println("$$$$$");
+			 System.out.println("&&&&&");
+		}
+
+	}
+
+}
