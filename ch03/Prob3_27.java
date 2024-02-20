a929d306d95febb8c8bc25e402499ff80d77656d
Dudeverse <se352@njit.edu>
2024-02-20 04:05:30 -0500

    add 3.26 and 3.27

---
+++ ch03/Prob3_27.java
@@ -0,0 +1,17 @@
+
+public class Prob3_27 {
+
+	public static void main(String[] args) {
+		int x = 8;
+		int y = 1;
+		
+		if (x > 5) {
+			if (y > 5) {
+				System.out.println("x and y are > 5");
+			} 
+		} else { 
+			System.out.println("x is <= 5");
+		}
+		
+	}
+}
