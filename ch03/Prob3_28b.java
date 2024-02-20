98c8bf958d3b72c8aa89c3f4b7df2bb6b774c780
Dudeverse <se352@njit.edu>
2024-02-20 04:06:16 -0500

    add 3.28b

---
+++ ch03/Prob3_28b.java
@@ -0,0 +1,19 @@
+
+public class Prob3_28b {
+
+	public static void main(String[] args) {
+		int x = 11;
+		int y= 9;
+		
+		if (x < 10) {
+			 if (y > 10) {
+				 System.out.println("*****");
+			 } else {
+				 System.out.println("#####");
+				 System.out.println("$$$$$");
+			 } 
+		}
+
+	}
+	
+}
