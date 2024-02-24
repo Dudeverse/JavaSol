8d8535c592a6aac82abac78b95ce5d57bdc7c064
Dudeverse <se352@njit.edu>
2024-02-24 08:35:15 -0500

    added 4.15a

---
+++ ch04/Prob4_15a.java
@@ -0,0 +1,14 @@
+
+public class Prob4_15a {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+			for (int j = 1; j <=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
