7f9666b37727f8723ed006fcb6ba19863ad49438
Dudeverse <se352@njit.edu>
2024-02-24 14:01:50 -0500

    added 4.19

---
+++ ch04/Prob4_19.java
@@ -0,0 +1,15 @@
+
+public class Prob4_19 {
+
+	public static void main(String[] args) {
+		int i=2, j=3, k=2 ,m=2;
+		System.out.println(i == 2);
+		System.out.println(j == 5);
+		System.out.println((i >= 0) && (j <= 3));
+		System.out.println((m <= 100) & (k <= m));
+		System.out.println((j >= i) || (k != m));
+		System.out.println((k + i < j) | (4 - j >= k));
+		System.out.println(!(k > j));
+	}
+
+}
