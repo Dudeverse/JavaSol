287c2d6997a41a000b2d6bce64d1056e3e4225cf
Dudeverse <se352@njit.edu>
2024-02-26 21:24:07 -0500

    added 4.27

---
+++ ch04/Prob4_27.java
@@ -0,0 +1,36 @@
+
+public class Prob4_27 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 3; j++) {
+				for (int k = 1; k <= 4; k++) {
+					System.out.print('*');
+				}
+				System.out.println();
+			}
+			 System.out.println();
+		} 
+	}
+
+}
+// The program outputs the following pattern:
+//****
+//****
+//****
+//
+//****
+//****
+//****
+//
+//****
+//****
+//****
+//
+//****
+//****
+//****
+//
+//****
+//****
+//****
