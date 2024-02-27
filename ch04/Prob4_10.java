ce21df3de305c812ca21c1cf74a342db08573f5c
Dudeverse <se352@njit.edu>
2024-02-26 21:16:53 -0500

    added 4.10

---
+++ ch04/Prob4_10.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Prob4_10 {
+		 public static void main( String[] args )
+			 {
+			 	Scanner s = new Scanner(System.in);
+			 	
+				 for (int i = 1; i < 3; i++)
+					 {
+					 for (int j = 1; j < 5; j++)
+						 System.out.print('*');
+						 System.out.println("\n#####");
+					 }// end outer for loop
+				 
+			 }// end main
+		 
+} // end class Counting
+
+// the program outputs the following pattern:
+//****
+//#####
+//****
+//#####
+
