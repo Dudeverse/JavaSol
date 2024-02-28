0f39346e1baedc7c0e44a9678e0b25935c03d675
Dudeverse <se352@njit.edu>
2024-02-28 18:53:25 -0500

    added 4.26

---
+++ ch04/Prob4_26.java
@@ -0,0 +1,17 @@
+
+public class Prob4_26 {
+
+	public static void main(String[] args) {
+		int count;
+		
+		for (count =1; count<5 && count<=10;count++) {// another way to implement break condition
+//			if (count==5) {
+//				break;
+//			}
+			System.out.printf("count: %d%n",count);
+			
+		}
+		System.out.printf("Early exit because of a break condition: %d%n",count);
+	}
+
+}
