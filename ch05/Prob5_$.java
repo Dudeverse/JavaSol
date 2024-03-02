d17f04c989dbaf2cdb50fbabfcccae7defbcecc0
Dudeverse <se352@njit.edu>
2024-03-02 14:18:17 -0500

    small section 5.3 example

--- ch05/Prob5_$.java
+++ ch05/Prob5_$.java
@@ -3,6 +3,8 @@ public class Prob5_$ {
 
 	public static void main(String[] args) {
 		// TODO PLAYGROUND FOR PROBLEMS OF CHAPTER 5
+		//Section 5.3 small example
+		System.out.println(Math.sqrt(900.00));
 
 	}
 
