736d2c3e0f8a9d6e33b02103b48bdd6c639a76bf
Dudeverse <se352@njit.edu>
2024-03-06 16:25:31 -0500

    added 5.7

---
+++ ch05/Prob5_7.java
@@ -0,0 +1,35 @@
+
+public class Prob5_7 {
+
+	public static void main(String[] args) {
+		// a)
+		double x = Math.abs(-7.5);
+		System.out.println(x);
+		
+		// b)
+		x = Math.floor(5 + 2.5);
+		System.out.println(x);
+		
+		// c)
+		x = Math.abs(9) + Math.ceil(2.2);
+		System.out.println(x);
+		
+		// d)
+		x = Math.ceil(-5.2);
+		System.out.println(x);
+		
+		// e)
+		x = Math.abs(-5) + Math.abs(4);
+		System.out.println(x);
+		
+		// f)
+		x = Math.ceil(-6.4) - Math.floor (5.2);
+		System.out.println(x);
+		
+		// g)
+		x = Math.ceil(-Math.abs(-3 + Math.floor(-2.5)));
+		System.out.println(x);
+		
+	}
+
+}
