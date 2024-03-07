02140cecc667af4a922d65b7aa00e74349150cb0
Dudeverse <se352@njit.edu>
2024-03-07 05:45:13 -0500

    added 5.20 modified

--- ch05/Prob5_20.java
+++ ch05/Prob5_20.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Prob5_20 {
 
 	public static void main(String[] args) {
-Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		
 		System.out.print("Enter a year: ");
 		int year = input.nextInt();
