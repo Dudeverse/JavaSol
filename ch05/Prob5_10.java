30f168b1a35c7cc23499841398518edeb8eb0dda
Dudeverse <se352@njit.edu>
2024-03-06 19:29:19 -0500

    added 5.10

---
+++ ch05/Prob5_10.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Prob5_10 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a real number: ");
+		double z = input.nextDouble();
+		
+		System.out.printf("%f rounded to the nearest integer is   : %d%n",z,roundToInteger(z));
+		System.out.printf("%f rounded to the nearest tenth is     : %.5f%n",z,roundToTenths(z));
+		System.out.printf("%f rounded to the nearest Hundredth is : %.5f%n",z,roundToHundredths(z));
+		System.out.printf("%f rounded to the nearest Thousandth is: %.5f%n",z,roundToThousandths(z));
+
+	}
+	
+	public static int roundToInteger(double x) {
+		double y = Math.floor(x + 0.5);
+		int i = (int) y;
+		return i;
+	}
+	
+	public static double roundToTenths(double x) {
+		double y = Math.floor(x * 10 + 0.5) / 10;
+		return y;
+	}
+	public static double roundToHundredths(double x) {
+		double y = Math.floor(x * 100 + 0.5) / 100;
+		return y;
+	}
+	public static double roundToThousandths(double x) {
+		double y = Math.floor(x * 1000 + 0.5) / 1000;
+		return y;
+	}
+}
