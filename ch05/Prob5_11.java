f4b87d8214901544a902dd094db033c0632f5224
Dudeverse <se352@njit.edu>
2024-03-07 05:15:36 -0500

    added 5.11

---
+++ ch05/Prob5_11.java
@@ -0,0 +1,17 @@
+
+public class Prob5_11 {
+
+	public static void main(String[] args) {
+		System.out.print("a) It means choosing a number from a set of numbers,\n where the probability of choosing is same for each and every number.\n");
+		System.out.println();
+		System.out.print("b) Because it chooses a number from a given range in\na 'non-deterministic' manner, that is there is no pattern followed nor\n a structure discovered when picking one number after the other.\n");
+		System.out.println();
+		System.out.print("c) Because the real world cases might require a particular\n set of values and does not require other sets. Dice requires 6 numbers,\n but coin toss requires only two numbers\n");
+		System.out.println();
+		System.out.print("d) My guess is that it is a great cost cutting technique. \nWhy waste unncessary human effort when computers can do the tedious calculations?\n");
+
+	
+	
+	}
+
+}
