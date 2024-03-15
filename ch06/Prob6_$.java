f06517079ec80a10aeca06830c9aa113170fa49b
Dudeverse <se352@njit.edu>
2024-03-15 06:13:56 -0400

    declared a couple of arrays

--- ch06/Prob6_$.java
+++ ch06/Prob6_$.java
@@ -3,7 +3,12 @@
 public class Prob6_$ {
 
 	public static void main(String[] args) {
-
+		int[] c = new int[12];
+		// declared my first java array!
+		
+		int[] d;
+		d = new int[12];
+		// declared my second java array!
 	}
 
 }
