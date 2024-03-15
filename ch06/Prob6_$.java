0cca887cf48da0424783948817f33b25f9c46412
Dudeverse <se352@njit.edu>
2024-03-15 06:40:43 -0400

    printed array with initializer

--- ch06/Prob6_$.java
+++ ch06/Prob6_$.java
@@ -3,6 +3,7 @@
 public class Prob6_$ {
 
 	public static void main(String[] args) {
+		System.out.println("Example-1"); // column headings
 		int[] c = new int[10]; // create array c
 		
 		System.out.printf("%s%8s%n", "Index", "Value"); // column headings
@@ -11,5 +12,21 @@ public class Prob6_$ {
 		for (int counter = 0; counter <c.length; counter++) {
 			System.out.printf("%5d%8d%n", counter, c[counter]);
 		}
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println("Example-2");
+		// using array initializer (sounds like an action film title)
+		int[] array = {32, 27, 64, 18, 95, 14, 90, 70, 60, 37};
+		
+		System.out.printf("%s%8s%n", "Index", "Value"); // column headings
+		
+		// output each array element's value
+		for (int counter = 0; counter <array.length; counter++) {
+			System.out.printf("%5d%8d%n", counter, array[counter]);
+		}
+		
+		
 	}
+		
 }
