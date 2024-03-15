88f6a4d1b9b030f0612b61ff186ea1076894b244
Dudeverse <se352@njit.edu>
2024-03-15 06:31:10 -0400

    printed array

--- ch06/Prob6_$.java
+++ ch06/Prob6_$.java
@@ -3,12 +3,13 @@
 public class Prob6_$ {
 
 	public static void main(String[] args) {
-		int[] c = new int[12];
-		// declared my first java array!
+		int[] c = new int[10]; // create array c
 		
-		int[] d;
-		d = new int[12];
-		// declared my second java array!
+		System.out.printf("%s%8s%n", "Index", "Value"); // column headings
+		
+		// output each array element's value
+		for (int counter = 0; counter <c.length; counter++) {
+			System.out.printf("%5d%8d%n", counter, c[counter]);
+		}
 	}
-
 }
