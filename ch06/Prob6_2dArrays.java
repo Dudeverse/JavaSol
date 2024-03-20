4f2954cd28b5164f3d6fe6bd695e7c91d8ccc13c
Dudeverse <se352@njit.edu>
2024-03-20 11:23:10 -0400

    finding totals

--- ch06/Prob6_2dArrays.java
+++ ch06/Prob6_2dArrays.java
@@ -3,7 +3,7 @@ public class Prob6_2dArrays {
 
 	public static void main(String[] args) {
 		int[][] array1 = {{1,2,3},{4,5,6}};
-		int[][] array2 = {{1,2} ,{3}, {4,5,6}};
+		int[][] array2 = {{3,3} ,{3}, {3,3,3}};
 		
 		System.out.println("Values in array1 by row are: ");
 		outputArray(array1);
@@ -14,11 +14,14 @@ public class Prob6_2dArrays {
 	}
 	
 	public static void outputArray(int[][] array) {
+		int total = 0;
 		for (int row = 0; row <array.length; row++) {
 			for (int col = 0; col <array[row].length; col++) {
 				System.out.printf("%d ",array[row][col]);
+				total = total + array[row][col]; 
 			}
 			System.out.println();
 		}
+		System.out.printf("sum of all elements in array is: %d%n",total);
 	}
 }
