ce51afb604bbef27d7f766e1767c56699ae582f0
Dudeverse <se352@njit.edu>
2024-03-20 11:13:08 -0400

    understanding traversal in 2d arrays

--- ch06/Prob6_2dArrays.java
+++ ch06/Prob6_2dArrays.java
@@ -2,16 +2,21 @@
 public class Prob6_2dArrays {
 
 	public static void main(String[] args) {
-		int[][] nice = {{1,2,3,4,5}, {6,7,8,9,10}};
+		int[][] array1 = {{1,2,3},{4,5,6}};
+		int[][] array2 = {{1,2} ,{3}, {4,5,6}};
 		
-		System.out.println("Values in array1 by row are");
-		outputArray(nice);
+		System.out.println("Values in array1 by row are: ");
+		outputArray(array1);
+		
+		System.out.printf("%nValues in array2 by row are: %n");
+		outputArray(array2);
+		
 	}
 	
 	public static void outputArray(int[][] array) {
-		for (int row = 0; row<array.length; row++) {
-			for(int column = 0; column<array[row].length; column++) {
-				System.out.printf("%d ", array[row][column]);
+		for (int row = 0; row <array.length; row++) {
+			for (int col = 0; col <array[row].length; col++) {
+				System.out.printf("%d ",array[row][col]);
 			}
 			System.out.println();
 		}
